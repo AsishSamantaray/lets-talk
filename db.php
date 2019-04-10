5b486009464eb41416213e4777e3e98502215ceb
AsishSamantaray <samantaray.asish04@gmail.com>
2019-04-10 16:48:36 +0530

    Database file added..

---
+++ db.php
@@ -0,0 +1,11 @@
+<?php
+    // DB connection..
+
+    $server_name = "localhost";
+    $user_name = "root";
+    $password = "root";
+    $db_name = "letstalk";
+
+    $con = mysqli_connect($server_name, $user_name, $password, $db_name);
+    
+?>
